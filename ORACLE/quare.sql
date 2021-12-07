f256f127de6b9b3429ba0f02f7f1272f60f644d3
geoV1RML <7099400+GeoTsik@users.noreply.github.com>
2021-12-07 15:01:31 +0200

    Add files via upload

--- ORACLE/quare.sql
+++ ORACLE/quare.sql
@@ -1,1 +1,23 @@
+prompt <== Creating table QUARE...;
 
+drop table QUARE cascade constraints;
+
+create table QUARE
+(ID integer not null);
+
+alter table QUARE add constraint quare_key#PK primary key(ID);
+
+prompt <== Creating table QUARE_DETAILS...;
+
+drop table QUARE_DETAILS;
+
+create table QUARE_DETAILS
+( ID integer not null
+, x1 integer not null
+, y1 integer not null
+, x2 integer not null
+, y2 integer not null
+);
+
+alter table QUARE_DETAILS add constraint quare_details_key#PK primary key(ID);
+alter table QUARE_DETAILS add constraint quare_details_fk#FK foreign key(ID) references QUARE(ID);
